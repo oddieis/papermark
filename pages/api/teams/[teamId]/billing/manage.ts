8aabf32bcf8569adfcd85138ea461aaff5631606
oddieis <oddieis@users.noreply.github.com>
2025-06-21 13:46:18 +0100

    Disable billing API endpoints

--- pages/api/teams/[teamId]/billing/manage.ts
+++ pages/api/teams/[teamId]/billing/manage.ts
@@ -1,22 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { stripeInstance } from "@/ee/stripe";
-import { getPriceIdFromPlan } from "@/ee/stripe/functions/get-price-id-from-plan";
-import { getQuantityFromPriceId } from "@/ee/stripe/functions/get-quantity-from-plan";
-import getSubscriptionItem from "@/ee/stripe/functions/get-subscription-item";
-import { PLANS, isOldAccount } from "@/ee/stripe/utils";
-import { waitUntil } from "@vercel/functions";
-import { getServerSession } from "next-auth/next";
-
-import { identifyUser, trackAnalytics } from "@/lib/analytics";
-import { errorhandler } from "@/lib/errorHandler";
-import prisma from "@/lib/prisma";
-import { CustomUser } from "@/lib/types";
-
-import { authOptions } from "../../../auth/[...nextauth]";
-
 export const config = {
-  // in order to enable `waitUntil` function
   supportsResponseStreaming: true,
 };
 
@@ -26,116 +10,8 @@ export default async function handle(
 ) {
   if (req.method === "POST") {
     return res.status(501).json({ error: "Billing disabled" });
-    /*
-    // POST /api/teams/:teamId/billing/manage – manage a user's subscription
-    const session = await getServerSession(req, res, authOptions);
-    if (!session) {
-      res.status(401).end("Unauthorized");
-      return;
-    }
-
-    const userId = (session.user as CustomUser).id;
-    const userEmail = (session.user as CustomUser).email;
-
-    const { teamId } = req.query as { teamId: string };
-    const {
-      priceId,
-      upgradePlan,
-      quantity,
-      addSeat,
-      proAnnualBanner,
-      return_url,
-    } = req.body as {
-      priceId: string;
-      upgradePlan: boolean;
-      quantity?: number;
-      addSeat?: boolean;
-      proAnnualBanner?: boolean;
-      return_url?: string;
-    };
-    try {
-      const team = await prisma.team.findUnique({
-        where: {
-          id: teamId,
-          users: {
-            some: {
-              userId: userId,
-            },
-          },
-        },
-        select: {
-          stripeId: true,
-          subscriptionId: true,
-          plan: true,
-        },
-      });
-
-      if (!team) {
-        return res.status(400).json({ error: "Team does not exists" });
-      }
-      if (!team.stripeId) {
-        return res.status(400).json({ error: "No Stripe customer ID" });
-      }
-
-      if (!team.subscriptionId) {
-        return res.status(400).json({ error: "No subscription ID" });
-      }
-
-      const subscriptionItemId = await getSubscriptionItem(
-        team.subscriptionId,
-        isOldAccount(team.plan),
-      );
-
-      const minQuantity = getQuantityFromPriceId(priceId);
-
-      const stripe = stripeInstance(isOldAccount(team.plan));
-      const { url } = await stripe.billingPortal.sessions.create({
-        customer: team.stripeId,
-        return_url: `${process.env.NEXTAUTH_URL}/settings/billing?cancel=true`,
-        ...((upgradePlan || addSeat) &&
-          subscriptionItemId && {
-            flow_data: {
-              type: "subscription_update_confirm",
-              subscription_update_confirm: {
-                subscription: team.subscriptionId,
-                items: [
-                  {
-                    id: subscriptionItemId,
-                    quantity: isOldAccount(team.plan)
-                      ? 1
-                      : (quantity ?? minQuantity),
-                    price: priceId,
-                  },
-                ],
-              },
-              after_completion: {
-                type: "redirect",
-                redirect: {
-                  return_url:
-                    return_url ??
-                    `${process.env.NEXTAUTH_URL}/settings/billing?success=true`,
-                },
-              },
-            },
-          }),
-      });
-
-      waitUntil(identifyUser(userEmail ?? userId));
-      waitUntil(
-        trackAnalytics({
-          event: "Stripe Billing Portal Clicked",
-          teamId,
-          action: proAnnualBanner ? "pro-annual-banner" : undefined,
-        }),
-      );
-
-      return res.status(200).json(url);
-    } catch (error) {
-      errorhandler(error, res);
-    }
-  */
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
+  res.setHeader("Allow", ["POST"]);
+  return res.status(405).end(`Method ${req.method} Not Allowed`);
 }
--- pages/api/teams/[teamId]/billing/upgrade.ts
+++ pages/api/teams/[teamId]/billing/upgrade.ts
@@ -1,18 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { stripeInstance } from "@/ee/stripe";
-import { getPlanFromPriceId, isOldAccount } from "@/ee/stripe/utils";
-import { waitUntil } from "@vercel/functions";
-import { getServerSession } from "next-auth/next";
-
-import { identifyUser, trackAnalytics } from "@/lib/analytics";
-import prisma from "@/lib/prisma";
-import { CustomUser } from "@/lib/types";
-
-import { authOptions } from "../../../auth/[...nextauth]";
-
 export const config = {
-  // in order to enable `waitUntil` function
   supportsResponseStreaming: true,
 };
 
@@ -22,110 +10,8 @@ export default async function handle(
 ) {
   if (req.method === "POST") {
     return res.status(501).json({ error: "Billing disabled" });
-    /*
-    // POST /api/teams/:teamId/billing/upgrade
-    const session = await getServerSession(req, res, authOptions);
-    if (!session) {
-      res.status(401).end("Unauthorized");
-      return;
-    }
-
-    const { teamId, priceId } = req.query as {
-      teamId: string;
-      priceId: string;
-    };
-
-    const { id: userId, email: userEmail } = session.user as CustomUser;
-
-    const team = await prisma.team.findUnique({
-      where: {
-        id: teamId,
-        users: {
-          some: {
-            userId,
-          },
-        },
-      },
-      select: { stripeId: true, plan: true },
-    });
-
-    if (!team) {
-      res.status(404).end("Unauthorized");
-      return;
-    }
-
-    const oldAccount = isOldAccount(team.plan);
-    const plan = getPlanFromPriceId(priceId, oldAccount);
-    const minimumQuantity = plan.minQuantity;
-
-    let stripeSession;
-
-    const lineItem = {
-      price: priceId,
-      quantity: oldAccount ? 1 : minimumQuantity,
-      ...(!oldAccount && {
-        adjustable_quantity: {
-          enabled: true,
-          minimum: minimumQuantity,
-          maximum: 99,
-        },
-      }),
-    };
-
-    const stripe = stripeInstance(oldAccount);
-    if (team.stripeId) {
-      // if the team already has a stripeId (i.e. is a customer) let's use as a customer
-      stripeSession = await stripe.checkout.sessions.create({
-        customer: team.stripeId,
-        customer_update: { name: "auto" },
-        billing_address_collection: "required",
-        success_url: `${process.env.NEXTAUTH_URL}/settings/billing?success=true`,
-        cancel_url: `${process.env.NEXTAUTH_URL}/settings/billing?cancel=true`,
-        line_items: [lineItem],
-        automatic_tax: {
-          enabled: true,
-        },
-        tax_id_collection: {
-          enabled: true,
-        },
-        mode: "subscription",
-        allow_promotion_codes: true,
-        client_reference_id: teamId,
-      });
-    } else {
-      // else initialize a new customer
-      stripeSession = await stripe.checkout.sessions.create({
-        customer_email: userEmail ?? undefined,
-        billing_address_collection: "required",
-        success_url: `${process.env.NEXTAUTH_URL}/settings/billing?success=true`,
-        cancel_url: `${process.env.NEXTAUTH_URL}/settings/billing?cancel=true`,
-        line_items: [lineItem],
-        automatic_tax: {
-          enabled: true,
-        },
-        tax_id_collection: {
-          enabled: true,
-        },
-        mode: "subscription",
-        allow_promotion_codes: true,
-        client_reference_id: teamId,
-      });
-    }
-
-    waitUntil(identifyUser(userEmail ?? userId));
-    waitUntil(
-      trackAnalytics({
-        event: "Stripe Checkout Clicked",
-        teamId,
-        priceId: priceId,
-      }),
-    );
-
-    return res.status(200).json(stripeSession);
-    */
-  } else {
-    // We only allow POST requests
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
+  res.setHeader("Allow", ["POST"]);
+  return res.status(405).end(`Method ${req.method} Not Allowed`);
 }
